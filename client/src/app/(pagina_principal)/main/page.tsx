af5ce94b19dce34933857510e3149b1e437ce81c
JPBaccarin <joaopedrobaccarin123@gmail.com>
2024-06-25 07:44:15 +0000

    update dos pacotes

---
+++ client/src/app/(pagina_principal)/main/page.tsx
@@ -0,0 +1,14 @@
+
+import MainSidebar from '@/components/main/sidebar'
+import React from 'react'
+
+function Main() {
+  return (
+        <main className='min-h-screen'>
+            <MainSidebar/>
+        </main>
+
+)
+}
+
+export default Main
--- client/src/components/main/sidebar.tsx
+++ client/src/components/main/sidebar.tsx
@@ -1,5 +1,5 @@
 // components/MainSidebar.tsx
-
+'use client'
 import { useState } from "react";
 import Link from "next/link";
 import {
