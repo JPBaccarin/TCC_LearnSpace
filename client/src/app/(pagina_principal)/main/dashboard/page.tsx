ca1f410297476ff8ad02cda1ee8dc1168d6199ff
padreqvedo <felipecoelhofreiredonascimento@gmail.com>
2024-06-27 19:42:22 -0300

    dashboard e speetch
    criação da dashboard e a fala de ingles
    
    Co-Authored-By: PedroH2OO <112864820+PedroH2OO@users.noreply.github.com>

--- client/src/app/(pagina_principal)/main/dashboard/page.tsx
+++ client/src/app/(pagina_principal)/main/dashboard/page.tsx
@@ -4,16 +4,19 @@ import QuestionChart from "@/components/dashboard/questionchart";
 import StatCard from "@/components/dashboard/StatCard";
 import SubjectReview from "@/components/dashboard/SubjectReview";
 import OtherFeatures from "@/components/dashboard/other_features";
-
+import { Bot } from "lucide-react";
 import MainSidebar from "@/components/main/sidebar";
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-900 p-4 text-white">
+    <div className="min-h-screen bg-gray-900 p-4 text-white ">
       <MainSidebar />
-      <h1 className="mb-4 text-2xl font-bold">Meu Desempenho</h1>
+      <div className=" px-20">
+           
+      <h1 className="mb-4 text-2xl font-bold">Meu Desempenho   </h1>
+      <div className="items my-2 flex flex-row gap-2 m-2"><Bot height={24}  /></div>  
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-        <div className="md:col-span-1">
+        <div className="md:col-span-1 px-1">
           <QuestionChart />
         </div>
         <div className="grid grid-cols-2 gap-4 md:col-span-2">
@@ -29,11 +32,12 @@ const Dashboard: React.FC = () => {
           />
         </div>
       </div>
-      <div className="mt-4">
+      <div className="mt-4 px-2">
         <SubjectReview />
       </div>
       <div className="mt-4">
         <OtherFeatures />
+      </div>
       </div>
     </div>
   );
--- client/src/components/speech/conversationchat.tsx
+++ client/src/components/speech/conversationchat.tsx
@@ -1,16 +1,27 @@
 import React from "react";
-import { Bot, BrainCog, MicOffIcon } from "lucide-react";
-import { Mic } from "lucide-react";
+import { Bot, BrainCog, MicOffIcon, Mic, AudioLines, Headset } from "lucide-react";
 import Image from "next/image";
 import faviconlogo from "@/app/favicon.ico";
 import { Button } from "../ui/button";
 
+
 function ConversationChat() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-background">
-      <div className="flex flex-row justify-between gap-4 w-full  rounded-md border p-4">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-background bgsvg px-96">
+      <div className="flex flew-row items-center absolute  top-0  py-24    ">
+        <div className="text-red-500">
+          <Headset
+          width={108}
+          height={108} />
+        </div>
+        <h1 className= "text-6xl">Speaking and Listening</h1>  
+      </div>
+        <div className="flex flex-row justify-between gap-4 w-full  rounded-lg border p-4 bg-background">
         <div className="flex flex-col items-center">
-          <div className="rounded-full bg-secondary p-4">
+          <div className="items my-2 flex flex-row gap-2 m-2">
+            <p className="">User</p>
+          </div>
+          <div className="rounded-full bg-secondary p-4 ">
             <Image
               src="https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg"
               width={108}
@@ -19,11 +30,33 @@ function ConversationChat() {
               className="rounded-full"
             />
           </div>
-          <div className="flex flex-row items-center gap-2">
-            <Button className=" border bg-secondary ">apertar pra falar</Button>
+          <div className="flex flex-row items-center">
+            <Button className=" border bg-background m-2">Apertar pra falar</Button>
+            <Mic />
           </div>
         </div>
+            
+        <div className="flex flex-row items-center justify-center">
+          
+        <AudioLines 
+          width={108}
+          height={108} 
+          />
+          <AudioLines 
+          width={108}
+          height={108} 
+          />
+          <AudioLines 
+          width={108}
+          height={108} 
+          />           
+        </div>        
+
         <div className="flex flex-col items-center">
+          <div className="items my-2 flex flex-row gap-2 m-2">
+            <p className="">LearnSpaceAI</p>
+            <Bot height={24} />
+          </div>
           <div className="rounded-full bg-primary p-4">
             <Image
               src={faviconlogo}
@@ -32,18 +65,24 @@ function ConversationChat() {
               alt="Disable Microphone"
             />
           </div>
-          <div className="items my-2 flex flex-row gap-2">
-            <p className="">LearnSpaceAI</p>
-            <Bot height={24} />
+          <div className="flex flex-row items-center">
+            <Button className=" border bg-background m-2">Parar tradução</Button>
+            <MicOffIcon stroke="red"/>
           </div>
         </div>
       </div>
 
-      <div className="w-full max-w-md rounded-lg bg-gray-800 p-4">
-        <div className="mb-2 text-xl font-bold">Feedback</div>
-        <div className="flex items-center space-x-2">
-          <div className="text-red-500">
-            Tente Melhorar a Pronúncia envolvendo palavras com a letra "R"
+      <div className="w-full rounded-lg bg-gray-800 p-4 m-4 ">
+        <div className="mb-2 text-xl font-bold ">Feedback</div>
+        <div className="flex items-center space-x-2 m-2 p-2">
+        <Image
+              src={faviconlogo}
+              width={48}
+              height={48}
+              alt="Disable Microphone"
+            />
+          <div className="text-white-500 flex flex-row items-center">
+            Tente Melhorar a Pronúncia envolvendo palavras com a letra "R" 
           </div>
         </div>
       </div>
